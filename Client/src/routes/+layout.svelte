4a143a7581e3a7036143313dfacc6b7b782abc1f
withyzu <withyzu@outlook.com>
2025-11-21 17:37:57 +0800

    feat: enhance project structure and UI components
    - Updated .vscode/settings.json to include additional files and directories for exclusion.
    - Modified Client/.vscode/settings.json for improved file management.
    - Refactored input components in Client/src/lib/components/ui/input/input.svelte to adjust focus ring styles.
    - Implemented layout changes in Client/src/routes/+layout.svelte to integrate Sidebar and Breadcrumb components.
    - Redesigned Client/src/routes/+page.svelte to include a login card with form elements.
    - Updated layout.css for improved styling and background adjustments.
    - Cleaned up DMS.sln by removing unnecessary project references.
    - Added new card components (Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, CardAction) for better UI structure.
    - Created label component with improved styling and functionality.

--- Client/src/routes/+layout.svelte
+++ Client/src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import AppSidebar from '$lib/components/explorer/app-sidebar.svelte';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 
 	let { children } = $props();
 </script>
@@ -9,4 +13,29 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		<header
+			class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
+		>
+			<div class="flex items-center gap-2 px-4">
+				<Sidebar.Trigger class="-ml-1" />
+				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+				<Breadcrumb.Root>
+					<Breadcrumb.List>
+						<Breadcrumb.Item class="hidden md:block">
+							<Breadcrumb.Link href="##">Building Your Application</Breadcrumb.Link>
+						</Breadcrumb.Item>
+						<Breadcrumb.Separator class="hidden md:block" />
+						<Breadcrumb.Item>
+							<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+						</Breadcrumb.Item>
+					</Breadcrumb.List>
+				</Breadcrumb.Root>
+			</div>
+		</header>
+
+		{@render children()}
+	</Sidebar.Inset>
+</Sidebar.Provider>
